c9cd95dfec88114bb14cb69462b66a978e76a42d
Bryan <chunkachung@gmail.com>
2022-05-21 14:51:15 +0800

    Fix fontAwesome bug

--- src/main.js
+++ src/main.js
@@ -3,9 +3,4 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-
-library.add(fas);
-
 createApp(App).use(store).use(router).mount("#app");
